6c36dba54d487aebd22070a9f190c0d1c86f5ef1
gyoergy.jandzso@t-systems.com <gyoergy.jandzso@t-systems.com>
2024-12-09 10:14:54 +0100

    Rename mainWindow

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace sap_hana_user_export
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new mainWindow());
         }
     }
 }
---
+++ mainWindow.cs
@@ -0,0 +1,15 @@
+namespace sap_hana_user_export
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
